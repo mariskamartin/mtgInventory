0e18c07ea7c114802e84c9c0396db890179b7d65
mar <mar@vera.cz>
2014-09-08 19:00:12 +0200

    odstraneni korekce

--- src/main/java/com/gmail/mariska/martin/mtginventory/listeners/DatabaseManager.java
+++ src/main/java/com/gmail/mariska/martin/mtginventory/listeners/DatabaseManager.java
@@ -1,7 +1,5 @@
 package com.gmail.mariska.martin.mtginventory.listeners;
 
-import java.util.List;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -11,13 +9,8 @@ import javax.servlet.ServletContextListener;
 
 import org.apache.log4j.Logger;
 
-import com.gmail.mariska.martin.mtginventory.db.CardDao;
 import com.gmail.mariska.martin.mtginventory.db.model.Card;
-import com.gmail.mariska.martin.mtginventory.db.model.CardEdition;
-import com.gmail.mariska.martin.mtginventory.db.model.CardRarity;
-import com.gmail.mariska.martin.mtginventory.service.CardService;
 import com.gmail.mariska.martin.mtginventory.utils.Utils;
-import com.google.common.collect.Lists;
 
 /**
  * Stara se o databazi a poskytuje EM.
@@ -36,27 +29,31 @@ public class DatabaseManager implements ServletContextListener {
         e.getServletContext().setAttribute("emf", emf);
         logger.info("database started");
 
-        correctDatabase(e.getServletContext());
+//        correctDatabase(e.getServletContext());
     }
 
-    private void correctDatabase(ServletContext servletContext) {
-        logger.info("Correct database - unknown cards");
-        EntityManager em = getEM(servletContext);
-        CardDao cardDao = new CardDao(em);
-        CardService cardService = new CardService(em);
-
-        List<Card> forDeleteList = Lists.newArrayList();
-        List<Card> deletedList = Lists.newArrayList();
-        forDeleteList.addAll(cardDao.findByEdition(CardEdition.UNKNOWN));
-        forDeleteList.addAll(cardDao.findByRarity(CardRarity.UNKNOWN));
-        for (Card card : forDeleteList) {
-            Card deleted = cardService.delete(card.getId());
-            if (deleted != null) {
-                deletedList.add(deleted);
-            }
-        }
-        logger.info("Deleted cards " + forDeleteList.size() + " / " + deletedList.size());
-    }
+    /**
+     * Korekce databaze kdy byly ulozeny i UNKNOWN karty
+     * @param servletContext
+     */
+//    private void correctDatabase(ServletContext servletContext) {
+//        logger.info("Correct database - unknown cards");
+//        EntityManager em = getEM(servletContext);
+//        CardDao cardDao = new CardDao(em);
+//        CardService cardService = new CardService(em);
+//
+//        List<Card> forDeleteList = Lists.newArrayList();
+//        List<Card> deletedList = Lists.newArrayList();
+//        forDeleteList.addAll(cardDao.findByEdition(CardEdition.UNKNOWN));
+//        forDeleteList.addAll(cardDao.findByRarity(CardRarity.UNKNOWN));
+//        for (Card card : forDeleteList) {
+//            Card deleted = cardService.delete(card.getId());
+//            if (deleted != null) {
+//                deletedList.add(deleted);
+//            }
+//        }
+//        logger.info("Deleted cards " + forDeleteList.size() + " / " + deletedList.size());
+//    }
 
     @Override
     public void contextDestroyed(ServletContextEvent e) {
