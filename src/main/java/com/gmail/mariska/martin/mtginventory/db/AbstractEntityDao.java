41e254c0acd5b9b4707fdfd2d85b4f9fdb9627d0
mar <mar@vera.cz>
2015-02-24 10:29:38 +0100

    refactor to durandal

---
+++ src/main/java/com/gmail/mariska/martin/mtginventory/db/AbstractEntityDao.java
@@ -0,0 +1,53 @@
+package com.gmail.mariska.martin.mtginventory.db;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Root;
+
+public abstract class AbstractEntityDao<T> implements IDao<T> {
+    private EntityManager em;
+    private Class<T> clazz;
+
+    public AbstractEntityDao(Class<T> clazz, EntityManager em) {
+        this.clazz = clazz;
+        this.em = em;
+    }
+
+    @Override
+    public List<T> getAll() {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<T> q = cb.createQuery(this.clazz);
+        Root<T> from = q.from(this.clazz);
+        return em.createQuery(q.select(from).orderBy(getDefaultOrder(cb, from))).getResultList();
+    }
+
+    @Override
+    public List<Order> getDefaultOrder(CriteriaBuilder cb, Root<T> from) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void insert(T entity) {
+        em.persist(entity);
+    }
+
+    @Override
+    public T update(T entity) {
+        return em.merge(entity);
+    }
+
+    @Override
+    public void delete(T entity) {
+        em.remove(entity);
+    }
+
+    @Override
+    public T findById(String entityId) {
+        return em.find(this.clazz, entityId);
+    }
+}
